7a468d09ac580f77bb6b30437867120207bf14c9
Ludovic <ludovic01.didelot@free.fr>
2017-01-04 10:27:14 +0100

    Second creation with the folders and the generated by intellj

---
+++ src/main/java/com/rbc/rbcone/hackaduck/DistributedRiskProfileApplication.java
@@ -0,0 +1,12 @@
+package com.rbc.rbcone.hackaduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedRiskProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedRiskProfileApplication.class, args);
+	}
+}
