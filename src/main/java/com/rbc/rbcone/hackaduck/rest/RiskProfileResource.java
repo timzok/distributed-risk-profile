39d804d78c0acb8f26e7f8cbb14ab58f37be9bfb
en129 <en129@rbc.com>
2017-01-05 11:54:00 +0100

    Bug fix

--- src/main/java/com/rbc/rbcone/hackaduck/rest/RiskProfileResource.java
+++ src/main/java/com/rbc/rbcone/hackaduck/rest/RiskProfileResource.java
@@ -74,10 +74,6 @@ public class RiskProfileResource {
     
     private LegalFund createLegalFund(String anId, String aName) {
     	LegalFund rslt = new LegalFund(anId, aName);
-    	LegalFund rslt = new LegalFund(anId, aName);
-    	LegalFund rslt = new LegalFund(anId, aName);
-    	/*rslt.setId(anId);
-    	rslt.setName(aName);*/
     	return rslt;
     }
 }
