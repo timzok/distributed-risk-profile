257e1f7864e83ace6345cbf383fedc64012e5a05
arnasrbc <Atax7ma1>
2017-01-09 14:55:37 +0100

    Added country data resource file.

---
+++ src/main/resources/static/js/countryData.js
@@ -0,0 +1,803 @@
+var countryData =
+{
+    "BF": {
+        "region": "AF",
+        "label": "Burkina Faso"
+    },
+    "DJ": {
+        "region": "AF",
+        "label": "Djibouti"
+    },
+    "BI": {
+        "region": "AF",
+        "label": "Burundi"
+    },
+    "BJ": {
+        "region": "AF",
+        "label": "Benin"
+    },
+    "ZA": {
+        "region": "AF",
+        "label": "South Africa"
+    },
+    "BW": {
+        "region": "AF",
+        "label": "Botswana"
+    },
+    "DZ": {
+        "region": "AF",
+        "label": "Algeria"
+    },
+    "ET": {
+        "region": "AF",
+        "label": "Ethiopia"
+    },
+    "RW": {
+        "region": "AF",
+        "label": "Rwanda"
+    },
+    "TZ": {
+        "region": "AF",
+        "label": "Tanzania"
+    },
+    "GQ": {
+        "region": "AF",
+        "label": "Eq. Guinea"
+    },
+    "NA": {
+        "region": "AF",
+        "label": "Namibia"
+    },
+    "NE": {
+        "region": "AF",
+        "label": "Niger"
+    },
+    "NG": {
+        "region": "AF",
+        "label": "Nigeria"
+    },
+    "TN": {
+        "region": "AF",
+        "label": "Tunisia"
+    },
+    "LR": {
+        "region": "AF",
+        "label": "Liberia"
+    },
+    "LS": {
+        "region": "AF",
+        "label": "Lesotho"
+    },
+    "ZW": {
+        "region": "AF",
+        "label": "Zimbabwe"
+    },
+    "TG": {
+        "region": "AF",
+        "label": "Togo"
+    },
+    "TD": {
+        "region": "AF",
+        "label": "Chad"
+    },
+    "ER": {
+        "region": "AF",
+        "label": "Eritrea"
+    },
+    "LY": {
+        "region": "AF",
+        "label": "Libya"
+    },
+    "GW": {
+        "region": "AF",
+        "label": "Guinea-Bissau"
+    },
+    "ZM": {
+        "region": "AF",
+        "label": "Zambia"
+    },
+    "CI": {
+        "region": "AF",
+        "label": "C“te d'Ivoire"
+    },
+    "EH": {
+        "region": "AF",
+        "label": "W. Sahara"
+    },
+    "CM": {
+        "region": "AF",
+        "label": "Cameroon"
+    },
+    "EG": {
+        "region": "AF",
+        "label": "Egypt"
+    },
+    "SL": {
+        "region": "AF",
+        "label": "Sierra Leone"
+    },
+    "CG": {
+        "region": "AF",
+        "label": "Congo"
+    },
+    "CF": {
+        "region": "AF",
+        "label": "Central African Rep."
+    },
+    "AO": {
+        "region": "AF",
+        "label": "Angola"
+    },
+    "CD": {
+        "region": "AF",
+        "label": "Dem. Rep. Congo"
+    },
+    "GA": {
+        "region": "AF",
+        "label": "Gabon"
+    },
+    "GN": {
+        "region": "AF",
+        "label": "Guinea"
+    },
+    "GM": {
+        "region": "AF",
+        "label": "Gambia"
+    },
+    "XS": {
+        "region": "AF",
+        "label": "Somaliland"
+    },
+    "CV": {
+        "region": "AF",
+        "label": "Cape Verde"
+    },
+    "GH": {
+        "region": "AF",
+        "label": "Ghana"
+    },
+    "SZ": {
+        "region": "AF",
+        "label": "Swaziland"
+    },
+    "MG": {
+        "region": "AF",
+        "label": "Madagascar"
+    },
+    "MA": {
+        "region": "AF",
+        "label": "Morocco"
+    },
+    "KE": {
+        "region": "AF",
+        "label": "Kenya"
+    },
+    "SS": {
+        "region": "AF",
+        "label": "S. Sudan"
+    },
+    "ML": {
+        "region": "AF",
+        "label": "Mali"
+    },
+    "KM": {
+        "region": "AF",
+        "label": "Comoros"
+    },
+    "ST": {
+        "region": "AF",
+        "label": "S?o Tom‚ and Principe"
+    },
+    "MW": {
+        "region": "AF",
+        "label": "Malawi"
+    },
+    "SO": {
+        "region": "AF",
+        "label": "Somalia"
+    },
+    "SN": {
+        "region": "AF",
+        "label": "Senegal"
+    },
+    "MR": {
+        "region": "AF",
+        "label": "Mauritania"
+    },
+    "UG": {
+        "region": "AF",
+        "label": "Uganda"
+    },
+    "SD": {
+        "region": "AF",
+        "label": "Sudan"
+    },
+    "MZ": {
+        "region": "AF",
+        "label": "Mozambique"
+    },
+    "BD": {
+        "region": "AS",
+        "label": "Bangladesh"
+    },
+    "MN": {
+        "region": "AS",
+        "label": "Mongolia"
+    },
+    "BN": {
+        "region": "AS",
+        "label": "Brunei"
+    },
+    "BH": {
+        "region": "AS",
+        "label": "Bahrain"
+    },
+    "BT": {
+        "region": "AS",
+        "label": "Bhutan"
+    },
+    "HK": {
+        "region": "AS",
+        "label": "Hong Kong"
+    },
+    "JO": {
+        "region": "AS",
+        "label": "Jordan"
+    },
+    "PS": {
+        "region": "AS",
+        "label": "Palestine"
+    },
+    "LB": {
+        "region": "AS",
+        "label": "Lebanon"
+    },
+    "LA": {
+        "region": "AS",
+        "label": "Lao PDR"
+    },
+    "TW": {
+        "region": "AS",
+        "label": "Taiwan"
+    },
+    "TR": {
+        "region": "AS",
+        "label": "Turkey"
+    },
+    "LK": {
+        "region": "AS",
+        "label": "Sri Lanka"
+    },
+    "TL": {
+        "region": "AS",
+        "label": "Timor-Leste"
+    },
+    "TM": {
+        "region": "AS",
+        "label": "Turkmenistan"
+    },
+    "TJ": {
+        "region": "AS",
+        "label": "Tajikistan"
+    },
+    "TH": {
+        "region": "AS",
+        "label": "Thailand"
+    },
+    "XC": {
+        "region": "AS",
+        "label": "N. Cyprus"
+    },
+    "NP": {
+        "region": "AS",
+        "label": "Nepal"
+    },
+    "PK": {
+        "region": "AS",
+        "label": "Pakistan"
+    },
+    "PH": {
+        "region": "AS",
+        "label": "Philippines"
+    },
+    "-99": {
+        "region": "AS",
+        "label": "Siachen Glacier"
+    },
+    "AE": {
+        "region": "AS",
+        "label": "United Arab Emirates"
+    },
+    "CN": {
+        "region": "AS",
+        "label": "China"
+    },
+    "AF": {
+        "region": "AS",
+        "label": "Afghanistan"
+    },
+    "IQ": {
+        "region": "AS",
+        "label": "Iraq"
+    },
+    "JP": {
+        "region": "AS",
+        "label": "Japan"
+    },
+    "IR": {
+        "region": "AS",
+        "label": "Iran"
+    },
+    "AM": {
+        "region": "AS",
+        "label": "Armenia"
+    },
+    "SY": {
+        "region": "AS",
+        "label": "Syria"
+    },
+    "VN": {
+        "region": "AS",
+        "label": "Vietnam"
+    },
+    "GE": {
+        "region": "AS",
+        "label": "Georgia"
+    },
+    "IL": {
+        "region": "AS",
+        "label": "Israel"
+    },
+    "IN": {
+        "region": "AS",
+        "label": "India"
+    },
+    "AZ": {
+        "region": "AS",
+        "label": "Azerbaijan"
+    },
+    "ID": {
+        "region": "AS",
+        "label": "Indonesia"
+    },
+    "OM": {
+        "region": "AS",
+        "label": "Oman"
+    },
+    "KG": {
+        "region": "AS",
+        "label": "Kyrgyzstan"
+    },
+    "UZ": {
+        "region": "AS",
+        "label": "Uzbekistan"
+    },
+    "MM": {
+        "region": "AS",
+        "label": "Myanmar"
+    },
+    "SG": {
+        "region": "AS",
+        "label": "Singapore"
+    },
+    "KH": {
+        "region": "AS",
+        "label": "Cambodia"
+    },
+    "CY": {
+        "region": "AS",
+        "label": "Cyprus"
+    },
+    "QA": {
+        "region": "AS",
+        "label": "Qatar"
+    },
+    "KR": {
+        "region": "AS",
+        "label": "Korea"
+    },
+    "KP": {
+        "region": "AS",
+        "label": "Dem. Rep. Korea"
+    },
+    "KW": {
+        "region": "AS",
+        "label": "Kuwait"
+    },
+    "KZ": {
+        "region": "AS",
+        "label": "Kazakhstan"
+    },
+    "SA": {
+        "region": "AS",
+        "label": "Saudi Arabia"
+    },
+    "MY": {
+        "region": "AS",
+        "label": "Malaysia"
+    },
+    "YE": {
+        "region": "AS",
+        "label": "Yemen"
+    },
+    "BE": {
+        "region": "EU",
+        "label": "Belgium"
+    },
+    "FR": {
+        "region": "EU",
+        "label": "France"
+    },
+    "BG": {
+        "region": "EU",
+        "label": "Bulgaria"
+    },
+    "DK": {
+        "region": "EU",
+        "label": "Denmark"
+    },
+    "HR": {
+        "region": "EU",
+        "label": "Croatia"
+    },
+    "DE": {
+        "region": "EU",
+        "label": "Germany"
+    },
+    "BA": {
+        "region": "EU",
+        "label": "Bosnia and Herz."
+    },
+    "HU": {
+        "region": "EU",
+        "label": "Hungary"
+    },
+    "JE": {
+        "region": "EU",
+        "label": "Jersey"
+    },
+    "FI": {
+        "region": "EU",
+        "label": "Finland"
+    },
+    "BY": {
+        "region": "EU",
+        "label": "Belarus"
+    },
+    "GR": {
+        "region": "EU",
+        "label": "Greece"
+    },
+    "RU": {
+        "region": "EU",
+        "label": "Russia"
+    },
+    "NL": {
+        "region": "EU",
+        "label": "Netherlands"
+    },
+    "PT": {
+        "region": "EU",
+        "label": "Portugal"
+    },
+    "NO": {
+        "region": "EU",
+        "label": "Norway"
+    },
+    "LI": {
+        "region": "EU",
+        "label": "Liechtenstein"
+    },
+    "LV": {
+        "region": "EU",
+        "label": "Latvia"
+    },
+    "LT": {
+        "region": "EU",
+        "label": "Lithuania"
+    },
+    "LU": {
+        "region": "EU",
+        "label": "Luxembourg"
+    },
+    "FO": {
+        "region": "EU",
+        "label": "Faeroe Is."
+    },
+    "PL": {
+        "region": "EU",
+        "label": "Poland"
+    },
+    "XK": {
+        "region": "EU",
+        "label": "Kosovo"
+    },
+    "CH": {
+        "region": "EU",
+        "label": "Switzerland"
+    },
+    "AD": {
+        "region": "EU",
+        "label": "Andorra"
+    },
+    "EE": {
+        "region": "EU",
+        "label": "Estonia"
+    },
+    "IS": {
+        "region": "EU",
+        "label": "Iceland"
+    },
+    "AL": {
+        "region": "EU",
+        "label": "Albania"
+    },
+    "IT": {
+        "region": "EU",
+        "label": "Italy"
+    },
+    "GG": {
+        "region": "EU",
+        "label": "Guernsey"
+    },
+    "CZ": {
+        "region": "EU",
+        "label": "Czech Rep."
+    },
+    "IM": {
+        "region": "EU",
+        "label": "Isle of Man"
+    },
+    "GB": {
+        "region": "EU",
+        "label": "United Kingdom"
+    },
+    "AX": {
+        "region": "EU",
+        "label": "Aland"
+    },
+    "IE": {
+        "region": "EU",
+        "label": "Ireland"
+    },
+    "ES": {
+        "region": "EU",
+        "label": "Spain"
+    },
+    "ME": {
+        "region": "EU",
+        "label": "Montenegro"
+    },
+    "MD": {
+        "region": "EU",
+        "label": "Moldova"
+    },
+    "RO": {
+        "region": "EU",
+        "label": "Romania"
+    },
+    "RS": {
+        "region": "EU",
+        "label": "Serbia"
+    },
+    "MK": {
+        "region": "EU",
+        "label": "Macedonia"
+    },
+    "SK": {
+        "region": "EU",
+        "label": "Slovakia"
+    },
+    "MT": {
+        "region": "EU",
+        "label": "Malta"
+    },
+    "SI": {
+        "region": "EU",
+        "label": "Slovenia"
+    },
+    "SM": {
+        "region": "EU",
+        "label": "San Marino"
+    },
+    "UA": {
+        "region": "EU",
+        "label": "Ukraine"
+    },
+    "SE": {
+        "region": "EU",
+        "label": "Sweden"
+    },
+    "AT": {
+        "region": "EU",
+        "label": "Austria"
+    },
+    "PR": {
+        "region": "NA",
+        "label": "Puerto Rico"
+    },
+    "DO": {
+        "region": "NA",
+        "label": "Dominican Rep."
+    },
+    "DM": {
+        "region": "NA",
+        "label": "Dominica"
+    },
+    "LC": {
+        "region": "NA",
+        "label": "Saint Lucia"
+    },
+    "NI": {
+        "region": "NA",
+        "label": "Nicaragua"
+    },
+    "PA": {
+        "region": "NA",
+        "label": "Panama"
+    },
+    "CA": {
+        "region": "NA",
+        "label": "Canada"
+    },
+    "SV": {
+        "region": "NA",
+        "label": "El Salvador"
+    },
+    "HT": {
+        "region": "NA",
+        "label": "Haiti"
+    },
+    "TT": {
+        "region": "NA",
+        "label": "Trinidad and Tobago"
+    },
+    "JM": {
+        "region": "NA",
+        "label": "Jamaica"
+    },
+    "GT": {
+        "region": "NA",
+        "label": "Guatemala"
+    },
+    "HN": {
+        "region": "NA",
+        "label": "Honduras"
+    },
+    "BZ": {
+        "region": "NA",
+        "label": "Belize"
+    },
+    "BS": {
+        "region": "NA",
+        "label": "Bahamas"
+    },
+    "CR": {
+        "region": "NA",
+        "label": "Costa Rica"
+    },
+    "US": {
+        "region": "NA",
+        "label": "United States"
+    },
+    "GL": {
+        "region": "NA",
+        "label": "Greenland"
+    },
+    "MX": {
+        "region": "NA",
+        "label": "Mexico"
+    },
+    "CU": {
+        "region": "NA",
+        "label": "Cuba"
+    },
+    "GU": {
+        "region": "OC",
+        "label": "Guam"
+    },
+    "PW": {
+        "region": "OC",
+        "label": "Palau"
+    },
+    "KI": {
+        "region": "OC",
+        "label": "Kiribati"
+    },
+    "NC": {
+        "region": "OC",
+        "label": "New Caledonia"
+    },
+    "NU": {
+        "region": "OC",
+        "label": "Niue"
+    },
+    "NZ": {
+        "region": "OC",
+        "label": "New Zealand"
+    },
+    "AU": {
+        "region": "OC",
+        "label": "Australia"
+    },
+    "PG": {
+        "region": "OC",
+        "label": "Papua New Guinea"
+    },
+    "SB": {
+        "region": "OC",
+        "label": "Solomon Is."
+    },
+    "PF": {
+        "region": "OC",
+        "label": "Fr. Polynesia"
+    },
+    "FJ": {
+        "region": "OC",
+        "label": "Fiji"
+    },
+    "FM": {
+        "region": "OC",
+        "label": "Micronesia"
+    },
+    "WS": {
+        "region": "OC",
+        "label": "Samoa"
+    },
+    "VU": {
+        "region": "OC",
+        "label": "Vanuatu"
+    },
+    "PY": {
+        "region": "SA",
+        "label": "Paraguay"
+    },
+    "CO": {
+        "region": "SA",
+        "label": "Colombia"
+    },
+    "VE": {
+        "region": "SA",
+        "label": "Venezuela"
+    },
+    "CL": {
+        "region": "SA",
+        "label": "Chile"
+    },
+    "SR": {
+        "region": "SA",
+        "label": "Suriname"
+    },
+    "BO": {
+        "region": "SA",
+        "label": "Bolivia"
+    },
+    "EC": {
+        "region": "SA",
+        "label": "Ecuador"
+    },
+    "AR": {
+        "region": "SA",
+        "label": "Argentina"
+    },
+    "GY": {
+        "region": "SA",
+        "label": "Guyana"
+    },
+    "BR": {
+        "region": "SA",
+        "label": "Brazil"
+    },
+    "PE": {
+        "region": "SA",
+        "label": "Peru"
+    },
+    "UY": {
+        "region": "SA",
+        "label": "Uruguay"
+    },
+    "FK": {
+        "region": "SA",
+        "label": "Falkland Is."
+    }
+};
